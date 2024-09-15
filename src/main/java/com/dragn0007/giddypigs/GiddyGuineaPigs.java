2fc41f3c793ec400b8cd12e6afccf0e82bbc6d40
kylie <cooper.kylie1077@gmail.com>
2024-09-15 01:10:51 -0500

    -Init

---
+++ src/main/java/com/dragn0007/giddypigs/GiddyGuineaPigs.java
@@ -0,0 +1,32 @@
+package com.dragn0007.giddypigs;
+
+import com.mojang.logging.LogUtils;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import org.slf4j.Logger;
+import software.bernie.geckolib3.GeckoLib;
+
+import static com.dragn0007.giddypigs.GiddyGuineaPigs.MODID;
+
+@Mod(MODID)
+public class GiddyGuineaPigs
+{
+
+    public static final Logger LOGGER = LogUtils.getLogger();
+    public static final String MODID = "giddypigs";
+
+    public GiddyGuineaPigs()
+    {
+        IEventBus eventBus = FMLJavaModLoadingContext.get().getModEventBus();
+
+//        GGGPItems.register(eventBus);
+//        GGPBlocks.register(eventBus);
+//        EntityTypes.ENTITY_TYPES.register(eventBus);
+//
+        GeckoLib.initialize();
+
+        MinecraftForge.EVENT_BUS.register(this);
+    }
+}
